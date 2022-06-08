3ff5a45349fb8027e20360eebd89c49021e32da7
cglotr <amir.ariffin.1992@gmail.com>
2022-06-08 15:02:57 +0800

    init prev & next

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello')
+import { Button } from './button'
+
+function main() {
+    console.log('LC+ has started.')
+    document.onkeydown = function (e) {
+        if (e.key === 'ArrowRight') {
+            const nextPageNode = document.querySelector(`li[title="Next Page"]`)
+            if (nextPageNode) {
+                const nextPageButton = <Button>(nextPageNode as any)
+                nextPageButton.click()
+            }
+        }
+        if (e.key === 'ArrowLeft') {
+            const prevPageNode = document.querySelector(`li[title="Previous Page"]`)
+            if (prevPageNode) {
+                const prevPageButton = <Button>(prevPageNode as any)
+                prevPageButton.click()
+            }
+        }
+    }
+}
+
+main()
